5d5640fa1647c09ead5e628ce964a085c9380eff
Gabs343 <gabriellopezp343@gmail.com>
2023-11-24 12:55:27 -0300

    method that forget the previous canvas and shows another

--- app.py
+++ app.py
@@ -2,14 +2,31 @@ import tkinter as tk
 import time
 import os
 
+from gui_factory import GUIFactory
+from canvas.principal_canvas import PrincipalCanvas
+
 class MainApp(tk.Tk):
     def __init__(self, *args, **kwargs) -> None:
-        tk.Tk.__init__(self, *args, **kwargs) 
+        tk.Tk.__init__(self, *args, **kwargs)
+        self.gui_factory: GUIFactory = GUIFactory()
         
         self.title("App")
         self.geometry("1036x565")
         self.resizable(False, False)
         self.configure(bg = "#112C5F")
+        
+        self.current_canvas = None
+        
+        self.canvas_objects = self.gui_factory.get_canvas(self, (PrincipalCanvas, ))
+        
+        self.show_canvas("PrincipalCanvas")
+        
+    def show_canvas(self, name: str):
+        if self.current_canvas:
+            self.current_canvas.pack_forget()
+            
+        self.current_canvas = self.canvas_objects[name]
+        self.current_canvas.pack()
         
 def main():
     app = MainApp()
