c0c83728bed90bfae86b471b312f352ad5f77ef8
Gabs343 <gabriellopezp343@gmail.com>
2023-11-24 13:15:08 -0300

    placing the subcanvas

--- canvas/principal_canvas.py
+++ canvas/principal_canvas.py
@@ -1,5 +1,13 @@
 import tkinter as tk
 
+from gui_factory import GUIFactory
+
 class PrincipalCanvas(tk.Canvas): 
     def __init__(self, parent, **kwargs) -> None:
         super().__init__(parent, **kwargs)
+        
+        self.parent = parent
+        self.gui_factory: GUIFactory = parent.gui_factory
+        
+        self.subcanvas = self.gui_factory.get_subcanvas(self)
+        self.subcanvas.place(x=75, y=75)
