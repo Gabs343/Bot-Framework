272d8d48d2438da5fc04e534ee9a6649a028b69e
Gabs343 <gabriellopezp343@gmail.com>
2023-11-24 16:52:03 -0300

    method that returns the bot container

--- gui_factory.py
+++ gui_factory.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 
+from components.bot_container_component import BotContainerComponent
+
 class GUIFactory:
     
     def get_canvas(self, parent: tk.Tk, classes: tuple) -> dict:
@@ -23,7 +25,7 @@ class GUIFactory:
                     highlightthickness = 0,
                     relief = "ridge")
         
-    def get_button(self, parent: tk.Canvas, icon: str, event) -> tk.Button:
+    def get_button(self, parent: tk.Canvas, icon: str, event=None) -> tk.Button:
         img = tk.PhotoImage(file=f'.\\assets\\icons\\{icon}.png')
         button = tk.Button(
             parent,
@@ -35,4 +37,14 @@ class GUIFactory:
         )
         button.image = img
         return button
+    
+    def get_bot_container(self, parent: tk.Canvas, coordinates: tuple, bot_instance) -> BotContainerComponent:
+        play_btn = self.get_button(parent=parent, icon="play", event=lambda: bot_instance.start()) 
+        setting_btn = self.get_button(parent=parent, icon="settings")
+        delete_btn = self.get_button(parent=parent, icon="delete")
+        
+        return BotContainerComponent(parent=parent,
+                                     coordinates=coordinates,
+                                     buttons= [play_btn, setting_btn, delete_btn],
+                                     bot=bot_instance)
     
