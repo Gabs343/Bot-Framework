42028e2db9c7544841bcff3275402dba569fcabc
Gabs343 <gabriellopezp343@gmail.com>
2023-11-24 15:21:43 -0300

    function that create new references for the bots

--- canvas/principal_canvas.py
+++ canvas/principal_canvas.py
@@ -8,23 +8,38 @@ class PrincipalCanvas(tk.Canvas):
         super().__init__(parent, **kwargs)
         
         self.parent = parent
+        self.bot_instances = {}
         self.gui_factory: GUIFactory = parent.gui_factory
         
         self.subcanvas = self.gui_factory.get_subcanvas(parent=self)
         self.subcanvas.place(x=75, y=75)
         
-        add_btn = self.gui_factory.get_button(parent=self, icon="add", event= lambda: print("add bot"))
+        add_btn = self.gui_factory.get_button(parent=self, icon='add', event= lambda: print('add bot'))
         add_btn.place(
             x=495.0, y=433.0,
             width=70.0, height=70.0
         )
         
-        refresh_btn = self.gui_factory.get_button(parent=self, icon="refresh", event= lambda: self.refresh())
+        refresh_btn = self.gui_factory.get_button(parent=self, icon='refresh', event= lambda: self.refresh())
         refresh_btn.place(
             x=915.0, y=29.0,
             width=50.0, height=50.0
         )
         
+        self.refresh()
+        
     def refresh(self) -> None:
         self.parent.create_bots_folder()
-        bots_folders = [folder_name for folder_name in os.listdir(self.parent.bots_folder_path) if os.path.join(self.parent.bots_folder_path, folder_name)]
+        bots_folders = [folder_name for folder_name in os.listdir(self.parent.bots_folder_path)]
+        new_instances = {}
+        
+        for folder in bots_folders:
+            module = __import__(f'bots.{folder}.main', fromlist=['Main'])
+            bot = getattr(module, 'Main')
+            
+            if(folder in self.bot_instances):
+                new_instances[folder] = self.bot_instances[folder]
+            else:
+                new_instances[folder] = bot()
+
+        self.bot_instances = new_instances
