d767f200b715f7133d922f7ceae5f4f76d05d2f0
Gabs343 <gabriellopezp343@gmail.com>
2023-11-24 12:54:23 -0300

    using super

--- canvas/principal_canvas.py
+++ canvas/principal_canvas.py
@@ -2,4 +2,4 @@ import tkinter as tk
 
 class PrincipalCanvas(tk.Canvas): 
     def __init__(self, parent, **kwargs) -> None:
-        tk.Canvas.__init__(parent, **kwargs)
+        super().__init__(parent, **kwargs)
