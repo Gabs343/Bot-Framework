a81d7b620420e458ebfaf5a290ad66052f8459b0
Gabs343 <gabriellopezp343@gmail.com>
2023-11-24 13:38:59 -0300

    refactor

--- canvas/principal_canvas.py
+++ canvas/principal_canvas.py
@@ -9,21 +9,17 @@ class PrincipalCanvas(tk.Canvas):
         self.parent = parent
         self.gui_factory: GUIFactory = parent.gui_factory
         
-        self.subcanvas = self.gui_factory.get_subcanvas(self)
+        self.subcanvas = self.gui_factory.get_subcanvas(parent=self)
         self.subcanvas.place(x=75, y=75)
         
         add_btn = self.gui_factory.get_button(parent=self, icon="add", event= lambda: print("add bot"))
         add_btn.place(
-            x=495.0,
-            y=433.0,
-            width=70.0,
-            height=70.0
+            x=495.0, y=433.0,
+            width=70.0, height=70.0
         )
         
         refresh_btn = self.gui_factory.get_button(parent=self, icon="refresh", event= lambda: print("reload container"))
         refresh_btn.place(
-            x=915.0,
-            y=29.0,
-            width=50.0,
-            height=50.0
+            x=915.0, y=29.0,
+            width=50.0, height=50.0
         )
