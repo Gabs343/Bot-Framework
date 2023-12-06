81999992bd8a634412a64401d713f0aadd847672
Gabs343 <gabriellopezp343@gmail.com>
2023-12-06 10:46:19 -0300

    removing method that deletes gui elements

--- components/bot_container_component.py
+++ components/bot_container_component.py
@@ -11,7 +11,6 @@ class BotContainerComponent:
         
         self.bot = bot
         self.buttons = buttons
-        self.gui_elements = []
         
         self.place_rectangles()
         self.place_information()
@@ -31,9 +30,6 @@ class BotContainerComponent:
             self.width+self.x_coordinate, self.height+self.y_coordinate,
             fill="#577190",
             outline="")
-        
-        self.gui_elements.append(shadow_id)
-        self.gui_elements.append(container_id)
         
     def place_information(self) -> None:
         bot_name_id = self.parent.create_text(
@@ -63,31 +59,17 @@ class BotContainerComponent:
             font=("RobotoRoman Regular", 16 * -1)
         )
         
-        self.gui_elements.append(bot_name_id)
-        self.gui_elements.append(text_id)
-        self.gui_elements.append(bot_status_id)
-        
     def place_buttons(self) -> None:                
         icon_x_coordinate = 661.0
         margin = 62.0
         for button in self.buttons:
-            window = self.parent.create_window(icon_x_coordinate, 
+            self.parent.create_window(icon_x_coordinate, 
                                     self.y_coordinate+30,
                                     width=45.0,
                                     height=45.0,
                                     window=button)
-            
-            self.gui_elements.append(button)
-            self.gui_elements.append(window)
           
             icon_x_coordinate += margin
-            
-    def remove_gui_elements(self) -> None:
-        for element in self.gui_elements:
-            if(type(element) is int):
-                self.parent.delete(element)
-            else:
-                element.destroy() 
             
     def set_button_event(self, button: int, event) -> None:
         self.buttons[button].config(command=event)
