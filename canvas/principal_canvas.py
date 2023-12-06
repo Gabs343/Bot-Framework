8afb672e0e8d9aa8fc6be176ef21511c4fae5374
Gabs343 <gabriellopezp343@gmail.com>
2023-12-06 10:43:55 -0300

    testing threads

--- canvas/principal_canvas.py
+++ canvas/principal_canvas.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import filedialog, messagebox
 import shutil
+import threading
 import os
 
 from gui_factory import GUIFactory
@@ -9,19 +10,21 @@ class PrincipalCanvas(tk.Canvas):
     def __init__(self, parent: tk.Tk, **kwargs) -> None:
         super().__init__(parent, **kwargs)
         
-        self.parent = parent
-        self.bot_instances = {}
-        self.bot_containers = []
-        self.gui_factory: GUIFactory = parent.gui_factory
+        self.__parent: tk.Tk = parent
+        self.__bot_instances: dict = {}
+        self.__gui_factory: GUIFactory = parent.gui_factory
         
-        self.subcanvas = self.gui_factory.get_subcanvas(parent=self)
+        self.create_gui_elements()
+        
+    def create_gui_elements(self) -> None:
+        self.subcanvas: tk.Canvas = self.__gui_factory.get_subcanvas(parent=self, dimensions=(897, 335))
         self.subcanvas.place(x=75, y=75)
         
-        add_btn = self.gui_factory.get_button(parent=self, 
+        add_btn: tk.Button = self.__gui_factory.get_button(parent=self, 
                                               icon='add', 
                                               event= lambda: self.add_bot())
         
-        refresh_btn = self.gui_factory.get_button(parent=self, 
+        refresh_btn: tk.Button = self.__gui_factory.get_button(parent=self, 
                                                   icon='refresh', 
                                                   event= lambda: self.refresh())
         
@@ -38,55 +41,56 @@ class PrincipalCanvas(tk.Canvas):
         self.refresh()
         self.create_bots_containers()
         
+    def create_bots_containers(self) -> None:
+        y = 8.0
+        for folder, bot_instance in self.__bot_instances.items():
+            
+            container = self.__gui_factory.get_bot_container(parent=self.subcanvas, 
+                                                           coordinates=(77.0, y), 
+                                                           bot_instance=bot_instance)
+            
+            container.set_button_event(button=0,
+                                       event=lambda bot=bot_instance: self.start_bot(bot=bot))
+            
+            container.set_button_event(button=1, 
+                                       event=lambda folder=folder, bot_instance=bot_instance: self.__parent.show_canvas(name='SettingCanvas',
+                                                                                                       bot_folder=folder,
+                                                                                                       settings=bot_instance.settings_services))
+            container.set_button_event(button=2, 
+                                       event=lambda folder=folder: self.delete_bot(folder=folder))
+            
+            y += 125.0
+            
     def refresh(self) -> None:
-        self.destroy_bots_containers()
-        self.parent.create_bots_folder()
-        bots_folders = [folder_name for folder_name in os.listdir(self.parent.bots_folder_path)]
+        self.subcanvas.delete("all")
+        self.__parent.create_bots_folder()
+        bots_folders = [folder_name for folder_name in os.listdir(self.__parent.bots_folder_path)]
         new_instances = {}
     
         for folder in bots_folders:
             module = __import__(f'bots.{folder}.main', fromlist=['Main'])
             bot = getattr(module, 'Main')
             
-            if(folder in self.bot_instances):
-                new_instances[folder] = self.bot_instances[folder]
+            if(folder in self.__bot_instances):
+                new_instances[folder] = self.__bot_instances[folder]
             else:
                 new_instances[folder] = bot()
 
-        self.bot_instances = new_instances
+        self.__bot_instances = new_instances
         self.create_bots_containers()
         
     def add_bot(self) -> None:
-        src_bot = filedialog.askdirectory()
-        shutil.move(src_bot, self.parent.bots_folder_path)
+        src_bot: str = filedialog.askdirectory()
+        shutil.move(src_bot, self.__parent.bots_folder_path)
         self.refresh()
-        
-    def create_bots_containers(self) -> None:
-        y = 8.0
-        for folder, bot_instance in self.bot_instances.items():
-            
-            container = self.gui_factory.get_bot_container(parent=self.subcanvas, 
-                                                           coordinates=(77.0, y), 
-                                                           bot_instance=bot_instance)
-            container.set_button_event(button=1, 
-                                       event=lambda folder=folder, bot_instance=bot_instance: self.parent.show_canvas(name='SettingCanvas',
-                                                                                                       bot_folder=folder,
-                                                                                                       settings=bot_instance.settings_services))
-            container.set_button_event(button=2, 
-                                       event=lambda folder=folder: self.delete_bot(folder=folder))
-            self.bot_containers.append(container)
-            y += 125.0
+    
+    def start_bot(self, bot) -> None:
+        thread = threading.Thread(target=bot.start)
+        thread.start()
             
     def delete_bot(self, folder: str) -> None:
-        msg_delete = messagebox.askquestion('Delete Bot', 'Are you sure you want to delete this bot?',
+        msg_delete: str = messagebox.askquestion('Delete Bot', 'Are you sure you want to delete this bot?',
                                 icon='warning')
         if(msg_delete == "yes"):
-            shutil.rmtree(f'{self.parent.bots_folder_path}\\{folder}')
+            shutil.rmtree(f'{self.__parent.bots_folder_path}\\{folder}')
         self.refresh()
-
-            
-    def destroy_bots_containers(self)  -> None:
-        for container in self.bot_containers:
-            container.remove_gui_elements()
-            
-        self.bot_containers.clear()
