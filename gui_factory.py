db274a7e09fe912acb5656f213fdd7b4315bed79
Gabs343 <gabriellopezp343@gmail.com>
2023-12-06 10:42:45 -0300

    method that place a text in the canvas

--- gui_factory.py
+++ gui_factory.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 
 from components.bot_container_component import BotContainerComponent
+from components.custom_entry import CustomInput
 
 class GUIFactory:
     
@@ -16,11 +17,11 @@ class GUIFactory:
                                         relief = "ridge")
         return canvas
     
-    def get_subcanvas(self, parent: tk.Canvas) -> tk.Canvas:
+    def get_subcanvas(self, parent: tk.Canvas, dimensions: tuple) -> tk.Canvas:
         return tk.Canvas(parent,
                     bg = "#112C5F",
-                    height = 335,
-                    width = 897,
+                    height = dimensions[1],
+                    width = dimensions[0],
                     bd = 0,
                     highlightthickness = 0,
                     relief = "ridge")
@@ -39,7 +40,7 @@ class GUIFactory:
         return button
     
     def get_bot_container(self, parent: tk.Canvas, coordinates: tuple, bot_instance) -> BotContainerComponent:
-        play_btn = self.get_button(parent=parent, icon="play", event=lambda: bot_instance.start()) 
+        play_btn = self.get_button(parent=parent, icon="play") 
         setting_btn = self.get_button(parent=parent, icon="settings")
         delete_btn = self.get_button(parent=parent, icon="delete")
         
@@ -47,4 +48,22 @@ class GUIFactory:
                                      coordinates=coordinates,
                                      buttons= [play_btn, setting_btn, delete_btn],
                                      bot=bot_instance)
+        
+    def get_custom_input(self, parent: tk.Canvas) -> CustomInput:
+        img = tk.PhotoImage(file='.\\assets\\icons\\entry.png')
+        return CustomInput(parent=parent,
+                           image=img,
+                           bd=0,
+                           bg="#CFE2FF",
+                           fg="#000716",
+                           highlightthickness=0)
+        
+    def set_text_in_canvas(self, parent: tk.Canvas, text: str, coordinates: tuple, size: int) -> int:
+        return parent.create_text(
+            coordinates[0], coordinates[1],
+            anchor="nw",
+            text=text,
+            fill="#FFFFFF",
+            font=("RobotoRoman ExtraBold", size * -1)
+        )
     
