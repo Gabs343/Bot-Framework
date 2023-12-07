ef8d80697a3762929e345c6af07baf15bb2903a9
Gabs343 <gabriellopezp343@gmail.com>
2023-12-07 19:28:49 -0300

    adding a scrollbar and some exceptions if there is a problem importing the module

--- canvas/principal_canvas.py
+++ canvas/principal_canvas.py
@@ -20,6 +20,12 @@ class PrincipalCanvas(tk.Canvas):
         self.subcanvas: tk.Canvas = self.__gui_factory.get_subcanvas(parent=self, dimensions=(897, 335))
         self.subcanvas.place(x=75, y=75)
         
+        self.scrollbar = tk.Scrollbar(self, orient=tk.VERTICAL, command=self.subcanvas.yview)
+        
+        self.subcanvas.configure(yscrollcommand=self.scrollbar.set)
+        
+        self.scrollbar.place(x= 70 +897, y=75, height=385)
+        
         add_btn: tk.Button = self.__gui_factory.get_button(parent=self, 
                                               icon='add', 
                                               event= lambda: self.add_bot())
@@ -27,6 +33,9 @@ class PrincipalCanvas(tk.Canvas):
         refresh_btn: tk.Button = self.__gui_factory.get_button(parent=self, 
                                                   icon='refresh', 
                                                   event= lambda: self.refresh())
+        
+        self.subcanvas.bind_all("<MouseWheel>", self.on_mousewheel)
+        self.subcanvas.bind("<Configure>", self.on_configure)
         
         add_btn.place(
             x=495.0, y=433.0,
@@ -40,6 +49,13 @@ class PrincipalCanvas(tk.Canvas):
         
         self.refresh()
         self.create_bots_containers()
+        
+    def on_configure(self, event=None) -> None:
+        self.subcanvas.configure(scrollregion=self.subcanvas.bbox("all"))
+
+    def on_mousewheel(self, event) -> None:
+        if event.delta:
+            self.subcanvas.yview_scroll(-1 * (event.delta // 120), "units")
         
     def create_bots_containers(self) -> None:
         y = 8.0
@@ -60,21 +76,27 @@ class PrincipalCanvas(tk.Canvas):
                                        event=lambda folder=folder: self.delete_bot(folder=folder))
             
             y += 125.0
-            
+    
     def refresh(self) -> None:
         self.subcanvas.delete("all")
         self.__parent.create_bots_folder()
-        bots_folders = [folder_name for folder_name in os.listdir(self.__parent.bots_folder_path)]
-        new_instances = {}
+        bots_folders: list[str] = [folder_name for folder_name in os.listdir(self.__parent.bots_folder_path)]
+        new_instances: dict = {}
     
         for folder in bots_folders:
-            module = __import__(f'bots.{folder}.main', fromlist=['Main'])
-            bot = getattr(module, 'Main')
-            
-            if(folder in self.__bot_instances):
-                new_instances[folder] = self.__bot_instances[folder]
-            else:
-                new_instances[folder] = bot()
+            try:
+                module = __import__(f'bots.{folder}.main', fromlist=['Main'])
+                bot = getattr(module, 'Main')
+                
+                if(folder in self.__bot_instances):
+                    new_instances[folder] = self.__bot_instances[folder]
+                else:
+                    new_instances[folder] = bot()
+                    
+            except ModuleNotFoundError:
+                messagebox.showerror(title='Main file not found', message='There is no file named main')
+            except AttributeError:
+                messagebox.showerror(title='Main not found', message=f'There is no Main class in {folder}')
 
         self.__bot_instances = new_instances
         self.create_bots_containers()
@@ -94,3 +116,5 @@ class PrincipalCanvas(tk.Canvas):
         if(msg_delete == "yes"):
             shutil.rmtree(f'{self.__parent.bots_folder_path}\\{folder}')
         self.refresh()
+        
+    
